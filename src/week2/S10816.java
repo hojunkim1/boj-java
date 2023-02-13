f043acce3c0d062b13867d7d717237ba172f68d4
Hojun Kim <76956067+hojunkim1@users.noreply.github.com>
2023-02-13 20:44:24 +0900

    Update 10816

--- src/week2/S10816.java
+++ src/week2/S10816.java
@@ -3,43 +3,68 @@ package week2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class S10816 {
+
+    static int upperBoundBinarySearch(int[] cards, int n, int target) {
+        int left = 0;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (target >= cards[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    static int lowerBoundBinarySearch(int[] cards, int n, int target) {
+        int left = 0;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (target <= cards[mid]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 해시맵
-        HashMap<Integer, Integer> map = new HashMap<>();
-
         // 입력 1
         int num1 = Integer.parseInt(br.readLine());
+        int[] cards = new int[num1];
         String[] nums1 = br.readLine().split(" ");
+        for (int i = 0; i < num1; i++)
+            cards[i] = Integer.parseInt(nums1[i]);
 
-        // 매핑
-        for (int i = 0; i < num1; i++) {
-            int num = Integer.parseInt(nums1[i]);
-            if (map.containsKey(num)) {
-                map.put(num, map.get(num) + 1);
-            } else {
-                map.put(num, 1);
-            }
-        }
+        // 정렬
+        Arrays.sort(cards);
 
         // 입력 2
         int num2 = Integer.parseInt(br.readLine());
+        int[] targets = new int[num2];
+        int[] result = new int[num2];
         String[] nums2 = br.readLine().split(" ");
+        for (int i = 0; i < num2; i++)
+            targets[i] = Integer.parseInt(nums2[i]);
+
+        // 탐색
+        for (int i = 0; i < num2; i++) {
+            int up = upperBoundBinarySearch(cards, num1, targets[i]);
+            int low = lowerBoundBinarySearch(cards, num1, targets[i]);
+            result[i] = up - low;
+        }
 
         // 출력
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < num2; i++) {
-            int num = Integer.parseInt(nums2[i]);
-            if (map.containsKey(num)) {
-                sb.append(map.get(num)).append(" ");
-            } else {
-                sb.append(0).append(" ");
-            }
-        }
-        System.out.println(sb);
+        for (int i = 0; i < num2; i++)
+            System.out.print(result[i] + " ");
     }
 }
--- src/week2/S1654.java
+++ src/week2/S1654.java
@@ -1,4 +1,8 @@
 package week2;
 
 public class S1654 {
+
+    public static void main(String[] args) {
+
+    }
 }
